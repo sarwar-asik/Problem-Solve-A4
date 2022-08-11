bcc171439c3a2374c9010ab42ae2d51663691532
sarwar-asik <shasik2021@gmail.com>
2022-08-11 23:46:44 +0600

    ready

---
+++ check.js
@@ -0,0 +1,115 @@
+                       /// // Problem-1 //////
+
+
+                       function radianToDegree(radian){
+                        if (typeof radian === 'number'){
+                            const degree = radian * (180/Math.PI);
+                            return degree.toFixed(2);
+                        }
+                        //this is for Valid variable ////
+                        else {
+                            return 'Valid !  Please Enter a Number';
+                        }
+                        }
+                        const radian  = radianToDegree(199);
+                                    //  Completed problem-1  /////
+                        
+                        
+                        
+                                                                    // //// Problem -2/////
+                        
+                        function  isJavaScriptFile(fileName){
+                            if (typeof fileName == 'string'){
+                                // for slice last 3 character ////
+                                const end3Charecter  =fileName.slice(-3); 
+                            // ///// for cheaking include **'.js'**///
+                                const isjsFile = end3Charecter.includes('.js');
+                                // / it will return bulean for file type//////
+                            return isjsFile;
+                            }
+                        ////////else is for Invalid or wrong input////
+                            else{
+                                return 'Invalid! Please Provide a String';
+                            }
+                        }
+                                        /////// Complete  Problem-2 ////
+                        
+                        
+                        
+                                                                 //// Problem-3////
+                        
+                        function  oilPrice(dieselAmount ,patrolAmount,octanAmount){
+                                // //  /this if condition is for number dataType////
+                            if (typeof dieselAmount == 'number' && typeof patrolAmount == 'number' && typeof octanAmount == 'number' ){
+                            
+                            // for per liter oil price //////
+                            const dieselPerLitre = 114;
+                            const patrolPerLitre = 130;
+                            const octanPerLitre = 135;
+                        
+                            const dieselCost = dieselPerLitre*dieselAmount;
+                            const patrolCost = patrolPerLitre*patrolAmount;
+                            const octanCost = octanPerLitre*octanAmount;
+                        
+                            const totalCost = dieselCost+patrolCost+octanCost;
+                        /////// Go>>>>return for total oilPrice  count////
+                        return totalCost;
+                            }
+                        ////for Invalid variable without number  ////
+                        else{
+                            return 'Invalid!   Please, Provide number in Amount .'  }
+                        }
+                                        //////completed Problem-3 /////
+                        
+                                                              ////  Problem-4  /////
+                        
+                        function publicBusFare(peopleNumber){
+                            let publicBusPercost = 250;
+                            let busCapacity = 50;
+                            let microCapacity = 11;
+                        // /////this if condition is for only number type //////
+                            if(typeof peopleNumber == 'number'){
+                            let busRemain = peopleNumber % busCapacity;
+                            // rest people from bus//
+                            let microRemain = busRemain%microCapacity;
+                            let publicBuscost = microRemain*publicBusPercost;
+                            return publicBuscost;
+                            }
+                            // for Invalid Number////
+                        else{
+                            return 'InValid! Please, Provide by Enter a Number.';
+                        }
+                        }
+                                        // //Problem-4 closed////
+                        
+                        
+                        
+                                               ////// // Problem -5///////
+                        
+                        function  isBestFriend(myself,abir){
+                            //// for bestFriend check ///
+                           const myNames = myself.name1;
+                           const abirNames = abir.name2;
+                        const myFriend = myself.friend1;
+                        const abirFriend = abir.friend2;
+                        
+                        // for valid vaiable //////
+                        const myselfType = typeof myself;
+                        const abirType = typeof abir;
+                        // / this condition is for == are they best friends ?////
+                        if(myNames ==abirFriend && abirNames==myFriend && myselfType== 'object'&& abirType=='object'){
+                            return true;
+                        }
+                        // ///this condition is for Invalid variable or input /////
+                        else if(myselfType != 'object' || abirType != 'object'){
+                            return 'Invalid ! Please Provide an Object'
+                        
+                        }
+                        // //this condition for they are not bestfriends////
+                        else{
+                             return false;
+                            }
+                        }
+                        //  problem -5  completed ///
+                        
+                        // /// *****  COMPLETE MY ASSIGNMENT-4 ****Thanks, Jankar vai! /////
--- index.js
+++ index.js
@@ -1,85 +1,75 @@
-//   node index.js /////
+                       /// // Problem-1 //////
 
-// Assignment -4 /////
 
-                        // Problem-1//////
-//   const typeRadianNumber = 199;
+function radianToDegree(radian){
+if (typeof radian === 'number'){
+    const degree = radian * (180/Math.PI);
+    return degree.toFixed(2);
+}
+//this is for Valid variable ////
+else {
+    return 'Valid !  Please Enter a Number';
+}
+}
+const radian  = radianToDegree(199);
+            //  Completed problem-1  /////
 
-// function radianToDegree(radian){
-// if (typeof radian === 'number'){
-//     const degree = radian * (180/Math.PI);
-//     return degree.toFixed(2);
-// }
-// //this is for Valid variable ////
-// else {
-//     return 'Valid !  Please Enter a Number';
-// }
-
-// }
-// const radian  = radianToDegree(typeRadianNumber);
-// console.log(radian);
-            //  Complete problem-1 remove call /////
 
 
                                             // //// Problem -2/////
-// const fileType ='index.js';
-// function  isJavaScriptFile(fileName){
-//     if (typeof fileName == 'string'){
-//         const end3Charecter  =fileName.slice(-3); 
-//         const isjsFile = end3Charecter.includes('.js');
-//     return isjsFile;
-//     }
-   
-//     else{
-//         return 'Invalid! Please Provide a String';
-//     }
-// }
-// const isjsFile = isJavaScriptFile(fileType);
-// console.log(isjsFile);
-// ///// Complete  Problem-2 but remove call////
+
+function  isJavaScriptFile(fileName){
+    if (typeof fileName == 'string'){
+        // for slice last 3 character ////
+        const end3Charecter  =fileName.slice(-3); 
+    // ///// for cheaking include **'.js'**///
+        const isjsFile = end3Charecter.includes('.js');
+        // / it will return bulean for file type//////
+    return isjsFile;
+    }
+//     //////else is for Invalid or wrong input////
+    else{
+        return 'Invalid! Please Provide a String';
+    }
+}
+                / ///// Complete  Problem-2 ////
 
 
 
                                          //// Problem-3////
 
-// const dieselAmount = 30;
-// const patrolAmount = 20;
-// const octanAmount = 10;
-// function  oilPrice(dieselAmount ,patrolAmount,octanAmount){
 
-//     if (typeof dieselAmount == 'number' && typeof patrolAmount == 'number' && typeof octanAmount == 'number' ){
+function  oilPrice(dieselAmount ,patrolAmount,octanAmount){
+        // //  /this if condition is for number dataType////
+    if (typeof dieselAmount == 'number' && typeof patrolAmount == 'number' && typeof octanAmount == 'number' ){
     
-//     // for per liter oil price //////
-//     const dieselPerLitre = 114;
-//     const patrolPerLitre = 130;
-//     const octanPerLitre = 135;
+    // for per liter oil price //////
+    const dieselPerLitre = 114;
+    const patrolPerLitre = 130;
+    const octanPerLitre = 135;
 
-//     const dieselCost = dieselPerLitre*dieselAmount;
-//     const patrolCost = patrolPerLitre*patrolAmount;
-//     const octanCost = octanPerLitre*octanAmount;
+    const dieselCost = dieselPerLitre*dieselAmount;
+    const patrolCost = patrolPerLitre*patrolAmount;
+    const octanCost = octanPerLitre*octanAmount;
 
-//     const totalCost = dieselCost+patrolCost+octanCost;
-// ///////Go >>>>. return for total cost count////
-// return totalCost;
-//     }
+    const totalCost = dieselCost+patrolCost+octanCost;
+/////// Go>>>>return for total oilPrice  count////
+return totalCost;
+    }
+////for Invalid variable without number  ////
+else{
+    return 'Invalid!   Please, Provide number in Amount .'  }
+}
 
-// ////for Invalid variable without number  ////
-// else{
-//     return 'Invalid!   Please, Provide number in Amount .'  }
-// }
-// const totalOil = oilPrice(dieselAmount,patrolAmount,octanAmount);
-// console.log(totalOil);
-// close Problem-3  remove call /////
+                //   ////completed Problem-3 /////
 
-                                          ////Problem-4/////
-
-let peopleNumber = 365;
+                                      ////  Problem-4/////
 
 function publicBusFare(peopleNumber){
     let publicBusPercost = 250;
     let busCapacity = 50;
     let microCapacity = 11;
-
+// /////this if condition is for only number type //////
     if(typeof peopleNumber == 'number'){
     let busRemain = peopleNumber % busCapacity;
     // rest people from bus//
@@ -89,45 +79,39 @@ function publicBusFare(peopleNumber){
     }
     // for Invalid Number////
 else{
-    return 'Valid! Please,  Enter a Number.';
+    return 'InValid! Please, Provide by Enter a Number.';
 }
 }
-let totalPeople = publicBusFare(peopleNumber);
-console.log(totalPeople);
-
-//// Problem-4 close////
+                // //Problem-4 closed////
 
 
 
-//   Problem -5///
-// // const aboutMy =20;
-// const aboutMy = {name:'sarwar',friend:'abir'};
-// // const aboutAbir = 10;
-// const aboutAbir = {name:'abir',friend:'sarwar'};
-// function  isBestFriend(myself,abir){
-//     //   for bestFriend check ///
-//    const myNames = myself.name;
-//    const abirNames = abir.name;
-// const myFriend = myself.friend;
-// const abirFriend = abir.friend;
+                       ////// // Problem -5///////
 
-// // for valid vaiable //////
-// const myselfType = typeof myself;
-// const abirType = typeof abir;
-// //this condition is for == are they best friends ?////
-// if(myNames ==abirFriend && abirNames==myFriend && myselfType== 'object'&& abirType=='object'){
-//     return true;
-// }
-// // this condition is for Invalid variable or input /////
-// else if(myselfType != 'object' || abirType != 'object'){
-//     return 'Invalid ! Please Provide an Object'
+function  isBestFriend(myself,abir){
+    //// for bestFriend check ///
+   const myNames = myself.name1;
+   const abirNames = abir.name2;
+const myFriend = myself.friend1;
+const abirFriend = abir.friend2;
 
-// }
-// // this condition for they are not bestfriends////
-// else{
-//      return false;
-//     }
-// }
-// const bestFriends = isBestFriend(aboutMy,aboutAbir);
-// console.log(bestFriends);
-//  problem -5  complete but remove please console and call///
+// for valid vaiable //////
+const myselfType = typeof myself;
+const abirType = typeof abir;
+// / this condition is for == are they best friends ?////
+if(myNames ==abirFriend && abirNames==myFriend && myselfType== 'object'&& abirType=='object'){
+    return true;
+}
+// ///this condition is for Invalid variable or input /////
+else if(myselfType != 'object' || abirType != 'object'){
+    return 'Invalid ! Please Provide an Object'
+
+}
+// //this condition for they are not bestfriends////
+else{
+     return false;
+    }
+}
+//  problem -5  completed ///
+
+// /// *****  COMPLETE MY ASSIGNMENT-4 ****Thanks, Jankar vai! /////
