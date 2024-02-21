000f134fbfcdf5711b12729352b277182151fa77
sarwar-asik <sarwarasik@gmail.com>
2024-02-21 18:54:35 +0600

    Practised_C++

---
+++ C++/first.cpp
@@ -0,0 +1,38 @@
+#include  <iostream>
+using namespace std ;
+
+int main (){
+
+cout << "sarwar hossain \n asik \n" << 3 << endl;
+
+cout << 1234 << endl; ///coment
+
+cout << "Types" << endl;
+
+int myNum = 10;
+
+cout << 'number' <<  myNum;
+
+char MyLetter = 'text type';
+
+cout << MyLetter;
+
+
+string myString ="my string";
+
+cout << myString;
+
+int x= 5 ;
+int y = 10 ;
+int sum = x +y ;
+
+cout << sum;
+
+bool isType = true;
+
+cout << isType;
+
+
+return 5;
+
+}
